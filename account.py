e5dd0cd7120a90fc2a67ab24b6a8f16a9faf3bb9
Lownickle <118482799+Lownickle@users.noreply.github.com>
2022-11-17 15:52:48 -0600

    account.py and test_account.py
    This is to make sure that I can actually upload the files

---
+++ account.py
@@ -0,0 +1,17 @@
+class Account:
+    def __init__(self, name):
+        __account_name = ''
+        __account_balance = ''
+
+    def deposit(self, amount):
+        a = ''
+
+    def withdraw(self, amount):
+        a = ''
+
+    def get_balance(self):
+        a = ''
+
+    def get_name(self):
+        a = ''
+        
